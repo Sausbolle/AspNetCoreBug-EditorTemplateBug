808d1a152d7dbaf41720d11715520eb594e6a575
Sausbolle <54682125+Sausbolle@users.noreply.github.com>
2021-08-27 17:10:44 +0200

    Initial Upload

---
+++ EditorTemplateBug/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using EditorTemplateBug.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EditorTemplateBug.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(FormModel formModel)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            return View();
+        }
+    }
+}
---
+++ EditorTemplateBug/ViewModels/FormModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EditorTemplateBug.ViewModels
+{
+    public class FormModel
+    {
+
+        [Required]
+        [Display(Name = "Some Text Input")]
+        public string TextInput { get; set; }
+
+        [Required]
+        [Display(Name = "Some Selected Option")]
+        public SelectOptionsEnum SelectedOption { get; set; }
+    }
+
+    public enum SelectOptionsEnum
+    {
+        SelectOne,
+        SelectTwo,
+        SelectThree,
+        SelectFour
+    }
+
+}
